f9fb10c688aff027a1ccdf69f501a1c408e7c0a7
adek312 <adrian.dolata@vp.pl>
2023-09-21 01:27:59 +0200

    poprawienie wpisywania wyboru przy wyswietlaniu i podejscie funkcyjne

--- pliki.py
+++ pliki.py
@@ -13,35 +13,37 @@ def main():
         except FileNotFoundError:
             print("Zły format")
     
-    
-
     # Przechowanie listy wszystkich plikow ze sciezki
     file_list = os.listdir(type_path)
-    
-    # Wybor co uzytkownik chce sprawdzic
-    print("Co chcesz sprawdzić:")
-    print("1- wielkość plików")
-    print("2- datę modyfikacji")
-    choice = input()
+    while True:
+        try:
+            print("Co chcesz sprawdzić:")
+            print("1- wielkość plików")
+            print("2- datę modyfikacji")
+            choice = int(input())
+            break
+        except ValueError:
+            print("Niepoprawny wybór")    
+    if choice == 1: list_size(file_list, type_path)
+    elif choice == 2: list_modification(file_list, type_path)
+    else: print("Niepoprawny wybór choice = {}".format(choice))
         
-        # Wylistowanie wszystkich plikow i ich wielkosci
-    if (choice == '1'):
-        for plik in file_list:
+            
+
+
+def list_size(file_list, type_path):
+    for plik in file_list:
             full_path = os.path.join(type_path, plik)
             size = os.path.getsize(full_path)
             megaOrGiga = megabytes_or_gigabytes(size)
             print('Plik {} waży: {}'.format(plik, megaOrGiga))
-               
-        # Wylistowanie wszystkich plikow i ich daty modyfikacji 
-    elif (choice == '2'):
-        for plik in file_list:
+
+def list_modification(file_list, type_path):
+    for plik in file_list:
             full_path = os.path.join(type_path, plik)
             mod_time = os.path.getmtime(full_path)
             mod_time_str = datetime.datetime.fromtimestamp(mod_time).strftime('%Y-%m-%d o godzinie: %H:%M:%S')
-            print('Plik {} użyty był: {}'.format(plik, mod_time_str))    
-            
-    else:
-        print("niepoprawny wybór")
+            print('Plik {} użyty był: {}'.format(plik, mod_time_str))
 
 # Sprawdzenie czy plik jest w GB czy MB
 def megabytes_or_gigabytes(bytes):
