ec31c68490f15f9a0234af733dbeba05976b34ec
adek312 <adrian.dolata@vp.pl>
2023-09-23 20:23:21 +0200

    zczytywanie wielkosci folderow

--- pliki.py
+++ pliki.py
@@ -1,9 +1,9 @@
 import os
 import datetime
-import pathlib
 
 def main():
     
+    #* Zczytanie sciezki
     while True:
         try:
             print("Podaj ścieżkę do folderu zgodnie ze wzorem: X:\\folder\\folder\\plik")
@@ -13,31 +13,53 @@ def main():
         except FileNotFoundError:
             print("Zły format")
     
-    # Przechowanie listy wszystkich plikow ze sciezki
+    #* Przechowanie listy wszystkich plikow ze sciezki
     file_list = os.listdir(type_path)
+    
+    #* Wybor do wylistowania
     while True:
         try:
             print("Co chcesz sprawdzić:")
             print("1- wielkość plików")
             print("2- datę modyfikacji")
+            print("3- test")
             choice = int(input())
             break
         except ValueError:
             print("Niepoprawny wybór")    
-    if choice == 1: list_size(file_list, type_path)
+    if choice == 1: file_size(file_list, type_path)
     elif choice == 2: list_modification(file_list, type_path)
-    else: print("Niepoprawny wybór choice = {}".format(choice))
-        
-            
+    elif choice == 3: sorting(file_list, type_path)
 
 
-def list_size(file_list, type_path):
+
+#* Wylistowanie wielkosci        
+def file_size(file_list, type_path):
     for plik in file_list:
-            full_path = os.path.join(type_path, plik)
+        full_path = os.path.join(type_path, plik)
+        # Folder
+        if  os.path.isdir(full_path): 
+            #? Funkcja directory_size sprawdza wielkość folderu w bajtach, która potem
+            #? Jest przesyłana do funkcji m_o_g, która sprawdza czy wielkość ma być
+            #? Podana w GB lub MB
+            megaOrGiga = megabytes_or_gigabytes(directory_size(full_path))
+            print('Plik {} waży: {}'.format(plik, megaOrGiga))
+        # Plik
+        else:
             size = os.path.getsize(full_path)
             megaOrGiga = megabytes_or_gigabytes(size)
             print('Plik {} waży: {}'.format(plik, megaOrGiga))
 
+#? Obliczanie wielkości folderu
+def directory_size(file_list):
+    bytes = 0
+    for main_path, katalogi, files in os.walk(file_list):
+        for plik in files:
+            plik_sciezka = os.path.join(main_path, plik)
+            bytes += os.path.getsize(plik_sciezka)
+    return bytes
+
+#* Wylistowanie czasu modyfikacji
 def list_modification(file_list, type_path):
     for plik in file_list:
             full_path = os.path.join(type_path, plik)
@@ -45,7 +67,15 @@ def list_modification(file_list, type_path):
             mod_time_str = datetime.datetime.fromtimestamp(mod_time).strftime('%Y-%m-%d o godzinie: %H:%M:%S')
             print('Plik {} użyty był: {}'.format(plik, mod_time_str))
 
-# Sprawdzenie czy plik jest w GB czy MB
+#TOdO Sortowanie plikow pod wzgledem wielkosci
+def sorting(file_list, type_path):
+    for plik in file_list:    
+        full_path = os.path.join(type_path, plik)
+        size = os.path.getsize(full_path)
+        megaOrGiga = megabytes_or_gigabytes(size)
+        print('Plik {} waży: {}'.format(plik, megaOrGiga))
+
+#* Sprawdzenie czy plik jest w GB czy MB
 def megabytes_or_gigabytes(bytes):
     if bytes >= 1024 ** 3: 
         Gbytes = bytes / (1024 ** 3)
@@ -54,6 +84,12 @@ def megabytes_or_gigabytes(bytes):
         Mbytes = bytes / (1024 ** 2)
         return f"{Mbytes:.2f} MB"
     
-# Wywolanie glownej funkcji przy starcie skryptu
+    
+
+
+
+
+
+#* Wywolanie glownej funkcji przy starcie skryptu
 if __name__ == "__main__":
     main()
